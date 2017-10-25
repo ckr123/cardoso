7b526888ca96f1af606f8f6f3ff2bf18c6d0161c
Christoffer Frydkjær <christoffer@uptime.dk>
2017-10-25 13:13:05 +0200

    added sitemap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Toscansk idyl til salg | cardoso.dk</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
